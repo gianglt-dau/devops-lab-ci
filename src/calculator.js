792b1fb8426cabefda4291a5a4f1315f5b0ca305
Giang Lam <gianglt@gmail.com>
2026-04-07 05:46:51 +0700

    Main

--- src/calculator.js
+++ src/calculator.js
@@ -17,11 +17,3 @@ export function divide(a, b) {
   }
   return a / b;
 }
-
-export function modulo(a, b) {
-  if (b === 0) {
-    throw new Error('Cannot modulo by zero');
-  }
-  return a % b;
-}
-
